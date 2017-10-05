31e677fb3dabc57b0be26200d811a4e2271314c5
waps12b <waps12b@naver.com>
2017-10-06 01:32:37 +0900

    add problem01A java solution

---
+++ Chapter01/Problem01A/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main{
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static int getMax(int a, int b)
+    {
+        if( a >= b ){
+            return a;
+        }else{
+            return b;
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        int p = scanner.nextInt();
+        int q = scanner.nextInt();
+        int answer = getMax(p, q);
+        System.out.println("%d\n", answer);
+    }
+
+}
