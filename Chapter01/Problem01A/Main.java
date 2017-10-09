ad0a035ee669864d13bfefa879638e6f6dbfc3f5
waps12b <waps12b@naver.com>
2017-10-10 02:47:15 +0900

    슬렉 테스트를 위한 커밋입니다

--- Chapter01/Problem01A/Main.java
+++ Chapter01/Problem01A/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Main{
     public static final Scanner scanner = new Scanner(System.in);
 
+    //a와 b중 더 큰 수를 반환하는 함수
     public static int getMax(int a, int b)
     {
         if( a >= b ){
