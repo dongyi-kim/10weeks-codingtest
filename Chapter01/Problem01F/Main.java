82abe6171ba745859e6aed56eac07264cd12755d
dongyi.kim <waps12b@naver.com>
2017-12-03 14:47:07 +0900

    Update Main.java

--- Chapter01/Problem01F/Main.java
+++ Chapter01/Problem01F/Main.java
@@ -1,10 +1,15 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 
 class Main {
-
+    public static final Scanner scanner = new Scanner(System.in);
+    
     public static int findValue(int value, int[] data) {
         int index = -1;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++)
+        {
 
             if (data[i] == value) {
                 index = i;
@@ -13,15 +18,19 @@ class Main {
         return index;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        int[] data = new int[n + 1];
-        for (int i = 0; i < n; i++) {
+        int[] data = new int[n];
+        
+        for (int i = 0; i < n; i++) 
+        {
             data[i] = scanner.nextInt();
         }
+        
         int answer = findValue(m, data);
+        
         System.out.println(answer);
     }
 }
