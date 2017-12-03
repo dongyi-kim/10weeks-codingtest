f100281338db0beeda05c5bf6997a6d2fb9c069c
dongyi.kim <waps12b@naver.com>
2017-12-03 14:54:12 +0900

    Update Main.java

--- Chapter01/Problem01I/Main.java
+++ Chapter01/Problem01I/Main.java
@@ -1,10 +1,17 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Main {
-    public static void selectionSort(int[] data) {
-        for (int i = 0; i < data.length; i++) {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void selectionSort(int[] data)
+    {
+        for (int i = 0; i < data.length; i++)
+        {
             int mini = i;
-            for (int j = i; j < data.length; j++) {
+            for (int j = i; j < data.length; j++)
+            {
                 if (data[mini] > data[j]) {
                     mini = j;
                 }
@@ -17,19 +24,24 @@ class Main {
 
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
         int n = scanner.nextInt();
         int[] data = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             data[i] = scanner.nextInt();
         }
+
         selectionSort(data);
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++)
+        {
             if (i > 0) {
                 System.out.print(" ");
             }
             System.out.print(data[i]);
         }
+
     }
 }
