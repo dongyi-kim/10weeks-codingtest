cfe4d0c80b00fb8259cc8590cf5c7177fdde28d0
dongyi.kim <waps12b@naver.com>
2017-12-03 14:38:59 +0900

    Update Main.java

--- Chapter01/Problem01B/Main.java
+++ Chapter01/Problem01B/Main.java
@@ -1,7 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*
+import java.io.*;
 
 class  Main {
-
+    public static final Scanner scanner = new Scanner(System.in);
+    
     public static int getMax(int[] data) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < data.length; i++) {
@@ -11,8 +14,8 @@ class  Main {
         return max;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
 
         int n = scanner.nextInt();
         int[] data = new int[n];
