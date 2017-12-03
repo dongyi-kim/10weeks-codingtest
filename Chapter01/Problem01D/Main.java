bd511e7c2be0a63f3999c382e8fa164ce3ba3ec6
dongyi.kim <waps12b@naver.com>
2017-12-03 14:41:56 +0900

    Update Main.java

--- Chapter01/Problem01D/Main.java
+++ Chapter01/Problem01D/Main.java
@@ -1,6 +1,11 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-class D1 {
+
+class Main
+{
+    public static final Scanner scanner = new Scanner(System.in);
 
     public static int getSum(int[] data)
     {
@@ -9,18 +14,20 @@ class D1 {
         for(int i = 0 ; i < data.length ; i++)
         {
             sum += data[i];
-
         }
         return sum;
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args)
+    {
         int n = scanner.nextInt();
         int[] data = new int[n];
+        
         for(int i = 0 ; i < n ; i ++)
         {
             data[i] = scanner.nextInt();
         }
+        
         int answer = getSum(data);
         System.out.println(answer);
     }
