5769d11ae449016d9d839277075a3f7218527478
dongyi.kim <waps12b@naver.com>
2017-12-03 14:48:48 +0900

    Update Main.java

--- Chapter01/Problem01G/Main.java
+++ Chapter01/Problem01G/Main.java
@@ -1,26 +1,31 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+class Main
+{
+    public static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args)
+    {
         int n = scanner.nextInt();
         int first_ajou = -1;
         int last_ajou = -1;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
+        
+        for (int i = 1; i <= n; i++)
+        {
             String name = scanner.next();
-            if (name.equals("AJOU")) {
-                if (first_ajou == -1) {
+            if (name.equals("AJOU"))
+            {
+                if (first_ajou == -1)
+                {
                     first_ajou = i;
-                } else {
-                    last_ajou = i;
                 }
-
+                last_ajou = i;
             }
         }
-
-
-        System.out.println(first_ajou + " " + last_ajou);
+        
+        System.out.printf("%d %d\n", first_ajou, last_ajou);
     }
 }
