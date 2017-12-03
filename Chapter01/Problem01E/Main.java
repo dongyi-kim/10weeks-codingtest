fb909c1a450680043bf00da1c998e189ec9be3bd
dongyi.kim <waps12b@naver.com>
2017-12-03 14:46:12 +0900

    Update Main.java

--- Chapter01/Problem01E/Main.java
+++ Chapter01/Problem01E/Main.java
@@ -1,23 +1,43 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 
 class Main {
+    public static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
         int count = 0;
         int sum = 0;
+
         int n = scanner.nextInt();
         int p = scanner.nextInt();
         int q = scanner.nextInt();
 
         int[] weight = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             weight[i] = scanner.nextInt();
-            if (weight[i] > p) {
+        }
+
+        for(int i = 0 ; i < n ; i ++)
+        {
+            if (weight[i] <= p)
+            {
                 count += 1;
                 sum += weight[i];
             }
         }
-        System.out.println(count + " " + sum);
+
+        System.out.printf("%d %d\n", count, sum);
+
+        if(sum <= q)
+        {
+            System.out.println("YES");
+        }else
+        {
+            System.out.println("NO");
+        }
     }
 }
