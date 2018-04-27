0e3bc6ed03dc93df3b05c860ac927c78eabe94a0
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 11:35:59 +0900

    Update c++ solution of 4B

--- Chapter04/Problem04B/solution.cpp
+++ Chapter04/Problem04B/solution.cpp
@@ -1,34 +1,71 @@
-#include<cstdio>
+#include <cstdio>
+#include <vector>
 
-const int MAX_N = 5000;
-const int MAX_M = 1000;
+using namespace std;
 
-int main()
-{
-	int n, m;
-	int used[MAX_N];
-	scanf("%d %d", &n, &m);
+class TicketTable {
+public:
+	vector<bool> used;
+	int length;
 
-	for (int ticket = 0; ticket < n; ticket++)
-	{
-		used[ticket] = 0;
+	TicketTable(int length) {
+		this->length = length;
+		this->used.assign(length, false);
 	}
 
-	for (int i = 0; i < m; i++)
-	{
-		int id;
-		scanf("%d", &id);
-		int ticket = id % n;
-
-		while (used[ticket] != 0)
-		{
-			ticket = (ticket + 1) % n;
+	/**
+	 * 사용자의 회원 번호로 지급받게 될 행운권 번호를 계산하는 메소드
+	 */
+	int findEmptyIndexByUserId(int userId) {
+		int index = userId % length;    // 가장 초기에 시도 할 티켓 번호
+		while (this->isEmpty(index) == true) {   // 사용된 티켓 번호라면 건너 뛴다
+			index = (index + 1) % length;   // 다음 번호를 조사해나간다.
 		}
-		
-		used[ticket] = 1;
-
-		printf("%d\n", ticket);
+		return index; // 사용되지 않은 인덱스를 찾아서 반환한다
 	}
 
-	return 0;
+	/**
+	 * 해당 행운권 번호가 이미 사용 중인지 여부를 반환하는 메소드
+	 */
+	bool isEmpty(int ticketIndex) {
+		return this->used[ticketIndex]; // 사용여부 <==> used[ticketIndex]
+	}
+
+	/**
+	 *  티켓 사용 여부를 갱신하기 위한 메소드
+	 */
+	void setUsed(int index, bool status) {
+		this->used[index] = status;
+	}
+};
+
+vector<int> getTicketNumbers(int n, int m, const vector<int>& ids) {
+	vector<int> tickets;
+	TicketTable table = TicketTable(n);
+
+	for(int i = 0; i < m; i ++) {
+		int userId = ids[i];
+		int ticketIndex = table.findEmptyIndexByUserId(userId);
+		tickets.push_back(ticketIndex);
+		table.setUsed(ticketIndex, true);
+	}
+
+	return tickets;
+}
+
+int main() {
+	// n: 전체 티켓의 수
+	// m: 요청 고객의 수
+	int n, m;
+	scanf("%d%d", &n, &m);
+
+	vector<int> ids(m);
+	for (int i = 0; i < m; ++i) {
+		scanf("%d", &ids[i]);
+	}
+
+	vector<int> tickets = getTicketNumbers(n, m, ids);
+	for (int i = 0; i < tickets.size(); ++i) {
+		printf("%d\n", tickets[i]);
+	}
 }
