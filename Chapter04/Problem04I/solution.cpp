621698aadb8630b7ae699b50feb6c5929c49e994
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 13:29:09 +0900

    Update c++ solution of 4I

--- Chapter04/Problem04I/solution.cpp
+++ Chapter04/Problem04I/solution.cpp
@@ -1,73 +1,78 @@
-#include<cstdio>
+#include <cstdio>
+#include <vector>
 
-const int MAX_N = 1000000;
+using namespace std;
 
+class Sieve {
+public:
+	int maximumValue;
+	vector<bool> isPrime;
+	vector<int> primes;
 
-bool isPrime[MAX_N + 1];
-void initSieve()
-{
-	for (int i = 1; i <= MAX_N; i += 1)
-	{
-		isPrime[i] = true;
+	Sieve(int maximumValue) {
+		this->maximumValue = maximumValue;
+		this->isPrime.assign(maximumValue + 1, true);
+		fillSieve();
 	}
 
-	isPrime[1] = false;
-	
-	for (int p = 2; p <= MAX_N; p += 1)
-	{
-		if (isPrime[p] == false)
-		{
-			continue;
-		}
 
-		for (long long m = (long long)p * p; m <= MAX_N; m += p)
-		{
-			isPrime[m] = false;
+	void fillSieve() {
+		isPrime[0] = isPrime[1] = false;
+		for(int num = 2; num <= maximumValue; num += 1) {
+			if(isPrime[num] == false){
+				continue;
+			}
+
+			primes.push_back(num);
+			for(long long mul = (long long)num * num; mul <= maximumValue; mul += num ) {
+				int index = (int)mul;
+				isPrime[index] = false;
+			}
 		}
 	}
-}
 
-void testCase(int caseIndex)
-{
-	int n;
-	scanf("%d", &n);
+	bool isPrimeNumber(int num) const {
+		return isPrime[num];
+	}
+};
 
-	int answerA = -1;
-	int answerB = -1;
+void process(int caseIndex, const Sieve& sieve) {
+	int x;
+	scanf("%d", &x);
 
-	for (int a = 3; a < n; a += 2)
-	{
-		if (isPrime[a] == false)
-		{
-			continue;
-		}
+	int a = -1, b = -1;
 
-		int b = n - a;
-		if (isPrime[b] == true)
-		{
-			answerA = a;
-			answerB = b;
+	// 두 수 p, q에 대해 소수 판별을 빠르게 하기 위해서
+	// 에라토스테네스의 체를 사용한다.
+	for(int p = 3; p <= x / 2; p += 2) {   
+		// p 보다 작은 모든 홀수 p에 대하여
+		int q = x - p; // x = p + q가 되는 q가 결정적으로 정해진다
+
+		// p와 q가 모두 소수라면?
+		if(sieve.isPrimeNumber(p) && sieve.isPrimeNumber(q))
+		{   // x = p + q가 되는 두 홀수 소수 <p, q>가 존재한다
+			a = p;
+			b = q;
 			break;
 		}
 	}
 
-	printf("Case #%d:\n", caseIndex + 1);
-    if(answerA == -1)
-    {
-        printf("-1\n");
-    }else{
-	    printf("%d = %d + %d\n", n, answerA, answerB);
-    }
+	printf("Case #%d:\n", caseIndex);
+	if (a > 0 && b > 0) {
+		printf("%d = %d + %d\n", x, a, b);
+	} else {
+		printf("-1\n");
+	}
 }
 
-int main()
-{
-	initSieve();
-	int caseNum;
-	scanf("%d", &caseNum);
-	for (int caseIndex = 0; caseIndex < caseNum; caseIndex += 1)
-	{
-		testCase(caseIndex);
+int main() {
+	const int MAX_VALUE = 1000000;
+	Sieve sieve(MAX_VALUE);
+
+	int caseSize;
+	scanf("%d", &caseSize);
+
+	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+		process(caseIndex, sieve);
 	}
-	return 0;
 }
