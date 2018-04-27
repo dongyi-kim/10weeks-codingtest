7c2df97d7148965092d750c76b157955a9b731cf
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 12:00:59 +0900

    Update c++ solution of 4F

--- Chapter04/Problem04F/solution.cpp
+++ Chapter04/Problem04F/solution.cpp
@@ -1,60 +1,86 @@
-#include<cstdio>
+#include <cstdio>
+#include <vector>
 
-const int MAX_N = 1000000;
-bool isPrime[MAX_N + 1];
+using namespace std;
 
-void initSieve()
-{
-	for (int i = 1; i <= MAX_N; i++)
-	{
-		isPrime[i] = true;
+//소인수 분해를 빠르게
+class Sieve {
+public:
+	int maximumValue;     // 에라토스테네스의 체에서 다룰 가장 큰 범위의 값
+	vector<bool> isPrime; // 각 숫자별 소수 여부
+	Sieve(int maximumValue) {
+		this->maximumValue = maximumValue;
+		this->isPrime.assign(maximumValue + 1, false);
+		this->fillSieve();
 	}
 
-	isPrime[1] = false;
+	/**
+	 *
+	 * @param num
+	 * @return 'num'이 소수라면 true, 그렇지 않으면 false
+	 */
+	bool isPrimeNumber(int num) const {
+		return this->isPrime[num];
+	}
 
-	for (int p = 2; p <= MAX_N; p++)
-	{
-		if (isPrime[p] == false)
-		{
-			continue;
+	/**
+	 * isPrime 배열의 값을 채우는 함수
+	 */
+	void fillSieve() {
+		isPrime.assign(this->maximumValue, true); // 처음에는 모두 소수라고 저장한다.
+		isPrime[0] = isPrime[1] = false;    // 0과 1은 소수가 아니므로 미리 처리한다.
+
+		for(int num = 2; num <= maximumValue; num += 1) {  
+			// [2, N] 사이의 모든 자연수 num에 대해
+			if( isPrime[num] == false ) {   
+				// 이미 소수가 아니라고 체크되었다면 건너 뛴다.
+				continue;
+			}
+
+			// 이 때 num은 소수다.
+			// 그러므로 num의 모든 배수를 소수가 아니라고 체크한다.
+			// num보다 작은 k에 대해 mul=num*num으로 표현되는 모든 자연수는 이미 처리되었다.
+			// 그러므로 mul = num * num 부터만 처리하면 된다.
+			for(long long mul = (long long)num * num; mul <= maximumValue; mul += num ) {   
+				// [!!] 이 때 mul에 대해서, mul을 나누는 '최초의 소인수 num'이 등장한다.
+				// 이 정보를 저장해두면 소인수 분해를 할 때 유용하다.
+				int index = (int)mul;
+				isPrime[index] = false;
+			}
 		}
+	}
+};
 
-		for (long long m = (long long) p * p; m <= MAX_N; m += p)
-		{
-			isPrime[m] = false;
+vector<int> getAllPrimeNumbers(int from, int to, const Sieve& sieve) {
+	vector<int> primes;
+
+	for(int num = from; num <= to; num += 1) {
+		if(sieve.isPrimeNumber(num)) {
+			primes.push_back(num);
 		}
 	}
 
+	return primes;
 }
 
-void testCase(int caseIndex)
-{
-	int l, r;
-	scanf("%d %d", &l, &r);
+void process(int caseIndex, const Sieve& sieve) {
+	int L, R;
+	scanf("%d%d", &L, &R);
 
-	int cnt = 0;
-	for (int p = l; p <= r; p+= 1)
-	{
-		if (isPrime[p] == true)
-		{
-			cnt += 1;
-		}
-	}
-	
-	printf("Case #%d:\n", caseIndex + 1);
-	printf("%d\n", cnt);
+	vector<int> allPrimeNumbers = getAllPrimeNumbers(L, R, sieve);
+
+	printf("Case #%d:\n", caseIndex);
+	printf("%d\n", (int)allPrimeNumbers.size());
 }
 
-int main()
-{
-	initSieve();
+int main() {
+	const int MAX_VALUE = 1000000;
+	Sieve sieve = Sieve(MAX_VALUE);
 
-	int caseNum;
-	scanf("%d", &caseNum);
-	for (int caseIndex = 0; caseIndex < caseNum; caseIndex += 1)
-	{
-		testCase(caseIndex);
+	int caseSize;
+	scanf("%d", &caseSize);
+
+	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+		process(caseIndex, sieve);
 	}
-	return 0;
 }
- 
