ff68e92a21da0283a21df660242d086b9ab79d08
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 11:32:38 +0900

    Update c++ solution of 4A

--- Chapter04/Problem04A/solution.cpp
+++ Chapter04/Problem04A/solution.cpp
@@ -1,43 +1,69 @@
-#include<cstdio>
+#include <cstdio>
 
-int getRowIndex(int index)
-{
-	return index / 9;
-}
+using namespace std;
 
-int getColIndex(int index)
-{
-	return index % 9;
-}
+const int MAX_ROW = 9;
+const int MAX_COL = 9;
 
-int getGroupIndex(int index)
-{
-	int r = getRowIndex(index);
-	int c = getColIndex(index);
-	int g = (r / 3) * 3 + (c / 3);
-	return g;
-}
 
-void testCase(int caseIndex)
-{
+class SudokuBoard{
+public:
+	// 칸의 번호로 행의 번호를 계산하는 메소드
+	int getRowByIndex(int index) {
+		// 칸의 번호에 대해 마디를 가지고 증가하므로 몫으로 계산할 수 있다.
+		return (index - 1) / 9 + 1;
+	}
+
+	// 칸의 번호로 열의 번호를 계산하는 메소드
+	int getColByIndex(int index) {
+		// 칸의 번호에 대해 규칙적으로 순환하므로 나머지로 계산할 수 있다.
+		return (index - 1) % 9 + 1;
+	}
+
+	// 칸의 번호로 그룹 번호를 계산하는 메소드
+	int getGroupByIndex(int index) {
+		int r = getRowByIndex(index);
+		int c = getColByIndex(index);
+		return getGroupByPosition(r, c);
+	}
+
+	// 칸의 위치 (행, 열)로 그룹 번호를 계산하는 메소드
+	int getGroupByPosition(int row, int column) {
+		// 행의 번호를 통해, 해당 행에 존재하는 그룹들 중 가장 왼쪽 그룹의 번호를 알 수 있다.
+		int left = ((row - 1) / 3) * 3 + 1;
+		// 열의 번호를 통해, 해당 행에 존재하는 그룹들 중 몇 번째 그룹에 속하는지 알 수 있다.
+		int offset = ((column - 1) / 3);
+		return left + offset;
+	}
+
+	// 칸의 위치 (행, 열)로 칸의 번호를 계산하는 메소드
+	int getIndexByPosition(int row, int column) {
+		// 행과 열 번호를 알면 반대로 인덱스도 쉽게 계산할 수 있다.
+		return (row - 1) * 9 + (column - 1) % 9 + 1;
+	}
+};
+
+
+void process(int caseIndex) {
 	int index;
 	scanf("%d", &index);
 
-	int r = 1 + getRowIndex(index - 1);
-	int c = 1 + getColIndex(index - 1);
-	int g = 1 + getGroupIndex(index - 1);
+	SudokuBoard board = SudokuBoard();
+
+	// 칸의 번호로 행, 열, 그룹 번호를 계산한다
+	int row = board.getRowByIndex(index);
+	int col = board.getColByIndex(index);
+	int group = board.getGroupByIndex(index);
 
 	printf("Case #%d:\n", caseIndex);
-	printf("%d %d %d\n", r, c, g);
+	printf("%d %d %d\n", row, col, group);
 }
 
-int main()
-{
-	int caseNum;
-	scanf("%d", &caseNum);
-	for (int caseIndex = 0; caseIndex < caseNum; caseIndex++)
-	{
-		testCase(caseIndex);
+int main() {
+	int caseSize;
+	scanf("%d", &caseSize);
+
+	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+		process(caseIndex);
 	}
-		return 0;
 }
