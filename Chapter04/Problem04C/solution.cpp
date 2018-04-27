03246bc3d40a86d776c10421412d4e54c45da0a5
Minho Cheon <mino@Minhoui-MacBook-Pro.local>
2018-04-27 11:37:50 +0900

    Update c++ solution of 4C

--- Chapter04/Problem04C/solution.cpp
+++ Chapter04/Problem04C/solution.cpp
@@ -1,41 +1,53 @@
-#include<cstdio>
+#include <cstdio>
 
-int gcd(int a, int b)
-{
-	int c = a % b;
-	while (c != 0)
-	{
-		a = b;
-		b = c;
-		c = a % b;
+using namespace std;
+
+
+/**
+ * a와 b의 최대 공약수를 계산하는 함수
+ *
+ * @param a
+ * @param b
+ * @return
+ */
+long long getGCD(long long a, long long b) {
+	while (a % b != 0)  {   // b가 a의 약수가 아니라면 반복한다
+		long long c = a % b; // 나머지를 계산한다
+		a = b;          // 이후 b와 c에 대해 이 과정을 반복한다
+		b = c;          // a <= b, b <= c
 	}
+	// b가 a의 약수라면, b가 a의 최대 공약수다
 	return b;
 }
 
-int lcm(int a, int b)
-{
-	return a * b / gcd(a, b);
+/**
+ * a와 b의 최소 공배수를 계산하는 함수
+ *
+ * @param a
+ * @param b
+ * @return
+ */
+long long getLCM(long long a, long long b) {
+	// 두 수의 곱에서 최대 공약수를 나누면 최소 공배수다
+	return a * b / getGCD(a, b);
 }
 
-void testCase(int caseIndex)
-{
-	int n, m;
-	scanf("%d %d", &n, &m);
+void process(int caseIndex) {
+	int num1, num2;
+	scanf("%d%d", &num1, &num2);
 
-	int g = gcd(n, m);
-	int l = lcm(n, m);
+	long long gcd =  getGCD(num1, num2);
+	long long lcm =  getLCM(num1, num2);
 
-	printf("Case #%d:\n", caseIndex + 1);
-	printf("%d %d\n", g, l);
+	printf("Case #%d:\n", caseIndex);
+	printf("%lld %lld\n", gcd, lcm);
 }
 
-int main()
-{
-	int caseNum;
-	scanf("%d", &caseNum);
-	for (int caseIndex = 0; caseIndex < caseNum; caseIndex += 1)
-	{
-		testCase(caseIndex);
+int main() {
+	int caseSize;
+	scanf("%d", &caseSize);
+
+	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+		process(caseIndex);
 	}
-	return 0;
 }
