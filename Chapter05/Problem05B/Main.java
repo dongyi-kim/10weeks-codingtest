30a5506caf1712bcf424c086369428910bf901b0
waps12b <waps12b@naver.com>
2018-05-02 03:03:18 +0900

    chapter 05 problem solution partially

---
+++ Chapter05/Problem05B/Main.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+	public static final Scanner scanner = new Scanner(System.in);
+
+	public static void testCase(int caseIndex) {
+		int N = scanner.nextInt();
+		int K = scanner.nextInt();
+
+		int[][] wastes = new int[N][N];
+		for (int r = 0; r < N; r += 1) {
+			for (int c = 0; c < N; c += 1) {
+				wastes[r][c] = scanner.nextInt();
+			}
+		}
+
+		int answer = Integer.MAX_VALUE;
+
+		for (int r = 0; r + K - 1 < N; r += 1) {
+			int minRow = r;
+			int maxRow = r + K - 1;
+			int sumOfWastes = 0;
+
+			for (int c = 0; c < N; c += 1) {
+				int minCol = c - K + 1;
+				int maxCol = c;
+
+				for(int i = minRow; i <= maxRow; i+= 1){
+					if(wastes[i][maxCol] >= 1){
+						sumOfWastes += 1;
+					}
+					if(wastes[i][minCol] >= 1){
+						sumOfWastes -= 1;
+					}
+				}
+
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+	public static void main(String[] args) throws Exception {
+		int caseSize = scanner.nextInt();
+
+		for (int caseIndex = 1; caseIndex <= caseSize; caseIndex += 1) {
+			testCase(caseIndex);
+		}
+
+	}
+
+}
---
+++ Chapter05/Problem05D/Main.java
@@ -0,0 +1,109 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+	public static final Scanner scanner = new Scanner(System.in);
+	public static final int[] deltaR = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	public static final int[] deltaC = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+
+	public static void testCase(int caseIndex) {
+		int N = scanner.nextInt();
+		int M = scanner.nextInt();
+
+		GameMap gameMap = new GameMap(N, N);
+
+		for (int r = 0; r < N; r += 1) {
+			for (int c = 0; c < N; c += 1) {
+				int buildings = scanner.nextInt();
+				gameMap.setBuildingsAt(r, c, buildings);
+			}
+		}
+
+		int answer = 0;
+
+		for(int i = 0 ; i < M ; i += 1){
+			int r = scanner.nextInt() - 1;
+			int c = scanner.nextInt() - 1;
+
+			int buildings = gameMap.getTotalPointAt(r, c);
+
+			answer = Math.max(answer, buildings);
+		}
+
+		System.out.println(answer);
+	}
+
+	public static void main(String[] args) throws Exception {
+		int caseSize = scanner.nextInt();
+
+		for (int caseIndex = 1; caseIndex <= caseSize; caseIndex += 1) {
+			testCase(caseIndex);
+		}
+	}
+
+}
+
+class GameMap{
+	public final int rows;
+	public final int columns;
+	private final int[][] buildings;
+
+	private final int[] rowSums;
+	private final int[] colSums;
+	private final int[] diagonalSumA, diagonalSumB;
+
+
+	public GameMap(int rows, int columns){
+		this.rows = rows;
+		this.columns = columns;
+		this.buildings = new int[rows][columns];
+		this.rowSums = new int[rows];
+		this.colSums = new int[columns];
+		this.diagonalSumA = new int[rows+columns];
+		this.diagonalSumB = new int[rows+columns];
+	}
+
+	public int getBuildingsAt(int r, int c){
+		if(this.isInside(r, c) == false)
+			return 0;
+
+		return this.buildings[r][c];
+	}
+
+	public int getTotalPointAt(int r, int c){
+		int buildings = 0;
+		buildings += this.rowSums[r];
+		buildings += this.colSums[c];
+		buildings += this.diagonalSumA[r+c];
+		buildings += this.diagonalSumB[c-r+this.rows-1];
+		buildings -= getBuildingsAt(r,c) * 3;
+		return buildings;
+	}
+
+	public void setBuildingsAt(int r, int c, int value){
+		int origin = getBuildingsAt(r, c);
+		int delta = value - origin;
+		this.buildings[r][c] = value;
+		this.rowSums[r] += delta;
+		this.colSums[c] += delta;
+		this.diagonalSumA[r+c] += delta;
+		this.diagonalSumB[c-r+this.rows-1] += delta;
+	}
+
+	public boolean isInside(int r, int c){
+		if(r < 0 || r >= this.rows)
+			return false;
+		if(c < 0 || c >= this.columns)
+			return false;
+		return true;
+	}
+
+
+
+
+
+
+}
+
