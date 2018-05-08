457161c3ddff61e991efd37d2b33375e09f664c6
waps12b <waps12b@naver.com>
2018-05-09 04:27:56 +0900

    add chapter05 problem M ~ problem P comments

--- Chapter05/Problem05M/Main.java
+++ Chapter05/Problem05M/Main.java
@@ -7,12 +7,15 @@ public class Main {
 	public static final Scanner scanner = new Scanner(System.in);
 	public static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static ArrayList<String> getMatchedStrings(String pattern, int N, String[] strings){
+	public static ArrayList<String> getMatchedStrings(String pattern, int N, String[] strings) {
 		ArrayList<String> matched = new ArrayList<>();
 
-		for(int i = 0 ; i < N; i+= 1){
+		for (int i = 0; i < N; i += 1) {
+			// 모든 문자열 S[i]에 대하여 
 			String s = strings[i];
-			if(StringUtil.isMatched(pattern, s)){
+
+			// 패턴과 일치하면 정답에 인덱스를 추가한다
+			if (StringUtil.isMatched(pattern, s)) {
 				matched.add(s);
 			}
 		}
@@ -25,13 +28,13 @@ public class Main {
 
 		String pattern = scanner.next();
 		String[] inputs = new String[N];
-		for(int i = 0; i < N ; i+= 1){
+		for (int i = 0; i < N; i += 1) {
 			inputs[i] = scanner.next();
 		}
 
 		ArrayList<String> matchedStrings = getMatchedStrings(pattern, N, inputs);
 
-		for(int i = 0; i < matchedStrings.size(); i+= 1){
+		for (int i = 0; i < matchedStrings.size(); i += 1) {
 			String s = matchedStrings.get(i);
 			writer.write(s);
 			writer.write("\n");
@@ -43,27 +46,33 @@ public class Main {
 
 }
 
-class StringUtil{
+class StringUtil {
 	public static final char ANY_CHARACTER = '?';
 
 	public static boolean isMatched(String pattern, String str) {
-		if(pattern.length() != str.length()){
+		// 글자수가 다르다면 매치될 수 가 없으므로 종료 
+		if (pattern.length() != str.length()) {
 			return false;
 		}
 
 		int N = pattern.length();
 		int matchedCount = 0;
-		for(int i = 0 ; i < N ; i += 1){
+		for (int i = 0; i < N; i += 1) {
+			// 각 i번째 글자 cp와 cs에 대하여 
 			char cp = pattern.charAt(i);
 			char cs = str.charAt(i);
-			if(cp == cs || cp == StringUtil.ANY_CHARACTER){
+
+			// cp와 cs가 일치하거나, cp가 '?' 와일드카드 인 경우 일치로 판단한다
+			if (cp == cs || cp == StringUtil.ANY_CHARACTER) {
 				matchedCount += 1;
-			}else{
+			} else {
+				// 하나라도 다른 문자가 등장한 경우 종료한다
 				break;
 			}
 		}
 
-		if(matchedCount == N){
+		// 모든 글자가 완전히 일치했다면 true를 반화한다
+		if (matchedCount == N) {
 			return true;
 		}
 		return false;
--- Chapter05/Problem05P/Main.java
+++ Chapter05/Problem05P/Main.java
@@ -6,30 +6,48 @@ import java.util.*;
 public class Main {
 	public static final Scanner scanner = new Scanner(System.in);
 
+	/**
+	 *
+	 * @param fullName  만들고자 하는 이름 문자열
+	 * @param N         사용할 수 있는 단어의 수
+	 * @param words     이름 제작에 사용할 수 있는 단어들 words[0] ~ words[N-1]
+	 * @return   서로 다른 세 개의 단어로 fullName을 만들 수 있다면 true, else false
+	 */
 	public static boolean isPossibleName(String fullName, int N, String[] words) {
+
+		// 서로 다른 i, j, k에 대해 (words[i] + words[j] + words[k])로 이름을 만들어보자 .
 		for (int i = 0; i < N; i += 1) {
+			// 모든 접두사 words[i]에 대해
 			String prefix = words[i];
+
+			// 만들고자 하는 이름이 prefix로 시작하지 않는다면 건너뛴다
 			if (fullName.startsWith(prefix) == false) {
 				continue;
 			}
 
 			for (int k = 0; k < N; k += 1) {
+				// 모든 접미사 words[k]에 대해
 				String suffix = words[k];
-				if (i == k
-						|| fullName.endsWith(suffix) == false)
-				{
+
+				// words[i]와 중복되거나 만들고자 하는 이름의 접미사가 아니라면 건너뛴다.
+				if (i == k || fullName.endsWith(suffix) == false) {
 					continue;
 				}
 
 				for (int j = 0; j < N; j += 1) {
+					// 중간 부분 word[sj]에 대해
 					String middle = words[j];
-					if( i == j || j == k )
-					{
+
+					// 이미 등장한 단어라면 건너뛴다
+					if (i == j || j == k) {
 						continue;
 					}
 
+					// 결정된 words[i] + words[j] + words[k]에 대하여
 					String concatenated = prefix + middle + suffix;
-					if(fullName.equals(concatenated)){
+
+					// 만들고자 하는 이름과 일치한다면 true를 반환한다
+					if (fullName.equals(concatenated)) {
 						return true;
 					}
 				}
@@ -49,9 +67,9 @@ public class Main {
 
 		boolean possible = isPossibleName(fullName, N, words);
 
-		if(possible){
+		if (possible) {
 			System.out.println("YES");
-		}else{
+		} else {
 			System.out.println("NO");
 		}
 	}
