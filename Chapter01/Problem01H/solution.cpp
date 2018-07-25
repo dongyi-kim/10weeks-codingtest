1e828049bc569082f1c78a8bba08d7cd714c03e6
jg7502 <37401109+jg7502@users.noreply.github.com>
2018-07-25 11:38:37 +0900

    Update solution.cpp
    printf("%d %d\n", answer, data[answer-1]);

--- Chapter01/Problem01H/solution.cpp
+++ Chapter01/Problem01H/solution.cpp
@@ -9,26 +9,22 @@ using namespace std;
  * 평균과의 차가 가장 작은 데이터의 번호를 반환하는 함수
  * 여러 가지라면 가장 빠른 번호를 반환한다.
  *
- * @param data 
- * @param n 
+ * @param data
+ * @param n
  * @return int  가장 평균과 가까운 데이터의 번호 (1번부터 시작)
  */
-int findIndex(int data[], int n)
-{
+int findIndex(int data[], int n) {
 	int x = 0; //존재하지 않는 경우는 없으므로, 일단 0이라고 가정
 	int S = 0; //모든 데이터의 합
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		S += data[i];
 	}
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		int dx = abs(n * data[x] - S); //i까지의 원소들 중 평균과의 최소거리
 		int di = abs(n * data[i] - S); //현재 원소와 평균과의 거리
-		if (di < dx)
-		{
+		if (di < dx) {
 			x = i;
 		}
 	}
@@ -36,21 +32,19 @@ int findIndex(int data[], int n)
 	return x + 1; //실제 번호는 1부터 시작하므로 증가
 }
 
-int main()
-{
+int main() {
 	int n;
-	int* data;
+	int *data;
 
 	scanf("%d", &n);
 	data = new int[n];
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &data[i]);
 	}
 
 	int answer = findIndex(data, n);
-	printf("%d\n", answer);
+	printf("%d %d\n", answer, data[answer-1]);
 
 	delete[] data;
 	return 0;
