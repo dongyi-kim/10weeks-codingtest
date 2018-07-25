6a605a3e5971f2938fc8ae09ceaa15f91d831d39
jg7502 <37401109+jg7502@users.noreply.github.com>
2018-07-25 11:31:15 +0900

    Update solution.cpp

--- Chapter01/Problem01A/solution.cpp
+++ Chapter01/Problem01A/solution.cpp
@@ -10,25 +10,21 @@ using namespace std;
 * @param b
 * @return a와 b중 더 큰 값
 */
-int getMax(int a, int b)
-{
-	if (a > b)
-	{
-		return a;
-	}
-	else{
-		return b;
-	}
+int getMax(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
 }
 
-int main()
-{
-	int p, q;
-	
-	scanf("%d %d", &p, q);
-    
+int main() {
+    int p, q;
+
+    scanf("%d %d", &p, &q);
+
     int answer = getMax(p, q);
 
     printf("%d\n", answer);
-	return 0;
+    return 0;
 }
