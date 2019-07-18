b3c9b8ec7e5212dbe51d6d98072f3cf6bd2382ec
dongyi.kim <dykim@codingmonster.net>
2019-07-18 14:27:51 +0900

    Update main.cpp

--- Chapter06/Problem06K/main.cpp
+++ Chapter06/Problem06K/main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 int main() {
 	int N;
 	cin >> N;
-
 	// 현재까지 최다득표한 후보들의 목록
 	vector<string> curMaxFreqName;
 
@@ -17,16 +16,12 @@ int main() {
 	map<string, int> frequencyMap;
 
 	int maxFrequency = 0; // 가장 많은 득표수
-
-	
-  //평소에 getline을 잘 안써서 모르겟으나 숫자 입력 줄의 엔터 영향으로 빈 문자열이 하나 들어온다.
-  //이를 제거하기 위해 getchar()
-	getchar();
   
   //각이름이하나추가될때마다 현재까지의 최다 특표 값 갱신, 최다 득표 후보 리스트를 갱신해간다.
   //vector의 clear메소드는 O(1)이다.
 	for (int i = 0; i<N; i++) {
-		string st; getline(cin, st);
+		string st;
+		cin >> st;
 		frequencyMap[st]++;
 		int k = frequencyMap[st];
 		if (k>maxFrequency) {
@@ -40,16 +35,16 @@ int main() {
 	}
 
 	// 최대 득표 후보 수를 출력한다.
-	cout << curMaxFreqName.size() << endl;
+	cout << maxFrequency << endl;
 
 	// 최대 득표를한 동점 후보들 이름을 사전순으로 출력한다.
 	sort(curMaxFreqName.begin(), curMaxFreqName.end());
+	bool first = true;
 	for (auto i : curMaxFreqName) {
-		cout << i << ' ';
+		if(first == false){
+			cout << ' ';
+		}
+		first = false;
+		cout << i;
 	}
-	printf("\n\n");
-	for (auto i : frequencyMap) {
-		cout << i.first << ' ' << i.second << '\n';
-	}
-	cout << endl;
 }
