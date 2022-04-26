c6e298f8e8a0875af2979b2b1850aac561094cb5
dongyi.kim <dykim@codingmonster.net>
2022-04-26 20:50:13 +0900

    Update Main.java

--- chapter08/problem_c/Main.java
+++ chapter08/problem_c/Main.java
@@ -19,18 +19,19 @@ public class Main {
 	 */
 	public static int getLongestPathLength(int current, int goal, ArrayList<Integer>[] adj){
 		int N = adj.length;
-
 		boolean[] visited = new boolean[N];
-
 		return getLongestPathLength(current, goal, 1, adj, visited);
 	}
 
+	public static final int NOPATH = -100;	// 경로가 없는 경우 길이를 음수로 표현하자 
 	public static int getLongestPathLength(int current, int goal, int depth, ArrayList<Integer>[] adj, boolean[] visited){
-		if(visited[current] == true || current == goal){
+		if( visited[current] == true ){	// 이미 방문한 노드라면 경로를 만들 수 없다 
+			return NOPATH;
+		} else if(current == goal){	// 목적지라면 0부터 역으로 길이를 가산해나가자
 			return 0;
 		}
 
-		int maxLength = 0;
+		int maxLength = NOPATH;
 
 		visited[current] = true;
 		for(int nextNode : adj[current]){
@@ -65,8 +66,11 @@ public class Main {
 		}
 
 		int answer = getLongestPathLength(origin, dest, adj);
-
-		System.out.println(answer);
+		if(answer < 0){
+			System.out.println(0); // 경로가 존재하지 않는다
+		}else{
+			System.out.println(answer);
+		}
 	}
 
 }
