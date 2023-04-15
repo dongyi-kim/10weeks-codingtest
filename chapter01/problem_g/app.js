45e6561ce0d5749828feae0d742f505fec4acb9f
ch.yun <ch.yun@catenoid.net>
2023-04-15 21:48:06 +0900

    1G

--- chapter01/problem_g/app.js
+++ chapter01/problem_g/app.js
@@ -1,22 +1,9 @@
-const input = [];
-require('readline')
-  .createInterface(process.stdin, {})
-  .on('line', function(line) {
-    input.push(line.trim());
-  })
-  .on('close', function() {
-    const n = parseInt(input[0]);
-    var first_ajou = -1;
-    var last_ajou = -1;
-    for (var i=1; i<n+1; i++)
-    {
-      if (input[i] == "AJOU") {
-        if (first_ajou == -1) {
-          first_ajou = i;
-        }
-        last_ajou = i;
-      }
-    }
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-		console.log(first_ajou + " " + last_ajou);
-	});
+input.shift();
+
+const match = [];
+input.filter((x, i) => x === "AJOU" && match.push(i + 1));
+
+console.log(match[0], match.at(-1));
