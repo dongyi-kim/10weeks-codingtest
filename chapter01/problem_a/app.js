d7d194beef47ca21bf5960e8e89eace516d2358c
ch.yun <ch.yun@catenoid.net>
2023-04-15 20:22:13 +0900

    1A

--- chapter01/problem_a/app.js
+++ chapter01/problem_a/app.js
@@ -1,20 +1,14 @@
-function getMax(a, b) {
-	if ( a >= b)
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const [a, b] = input[0].split(" ").map(Number);
+
+const getMax = (a, b) => {
+	if(a > b) {
 		return a;
-	else
+	} else {
 		return b;
-}
+	}
+};
 
-const input = [];
-
-require('readline')
-  .createInterface(process.stdin, {})
-  .on('line', function(line) {
-    input.push(line.trim());
-  })
-  .on('close', function() {
-		const values = input[0].split(" ").map(Number);
-		const a = values[0];
-		const b = values[1];
-		console.log(getMax(a,b));
-	});
+console.log(getMax(a,b));
