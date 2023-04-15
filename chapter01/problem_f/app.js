e3ca80970f1d3c773cad8d3f40a61301925695f0
ch.yun <ch.yun@catenoid.net>
2023-04-15 21:24:50 +0900

    1F

--- chapter01/problem_f/app.js
+++ chapter01/problem_f/app.js
@@ -1,26 +1,8 @@
-function find_value(value, data, length)
-{
-  var index = -1;
-  for (var i=0; i<length; i++)
-  {
-    if (value == data[i]) {
-      index = i;
-    }
-  }
-  return index;
-}
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const input = [];
-require('readline')
-  .createInterface(process.stdin, {})
-  .on('line', function(line) {
-    input.push(line.trim());
-  })
-  .on('close', function() {
-    const values = input[0].split(" ").map(Number);
-    const data = input[1].split(" ").map(Number);
-    const n = values[0];
-    const m = values[1];
+const [_, M] = input[0].split(" ").map(Number);
+const data = input[1].split(" ").map(Number);
 
-		console.log(find_value(m, data, n));
-	});
+const answer = data.findIndex(x => M === x);
+console.log(answer);
