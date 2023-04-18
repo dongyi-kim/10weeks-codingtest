38a0e566ac688502ff9a1332efcb8aa6f71dc98b
Sherry90 <hhlsdkm@gmail.com>
2023-04-18 16:28:28 +0900

    1H

--- chapter01/problem_h/app.js
+++ chapter01/problem_h/app.js
@@ -1,39 +1,16 @@
-function find_value(value, data, length)
-{
-  var index = -1;
-  for (var i=0; i<length; i++)
-  {
-    if (value == data[i]) {
-      index = i;
-    }
-  }
-  return index;
-}
+const fs = require('fs');
+const input = (()=>{
+  const stdin = fs.readFileSync('/dev/stdin').toString().split('\n');
+  let ln=0;
+  return ()=>stdin[ln++];
+})();
 
-const input = [];
-require('readline')
-  .createInterface(process.stdin, {})
-  .on('line', function(line) {
-    input.push(line.trim());
-  })
-  .on('close', function() {
-    const n = parseInt(input[0]);
-    const data = input[1].split(" ").map(Number);
-    var index = -1, answer = 0, average = 0, sum = 0;
-    var minGap = Number.MAX_SAFE_INTEGER;
-    for (var i=0; i<n; i++)
-    {
-      sum += data[i];
-    }
-    average = sum / n;
-    for (var i=0; i<n; i++)
-    {
-      var gap = Math.abs(average - data[i]);
-      if (gap < minGap) {
-        minGap = gap;
-        index = i+1;
-        answer = data[i];
-      }
-    }
-		console.log(index + " " + answer);
-	});
+const n = Number(input());
+const data = input().split(" ").map(Number); // 두 번째 라인의 입력받은 숫자들을 숫자 배열로 저장
+
+const sum = data.reduce((acc,cur) => acc + cur,0); // 총합
+const diff = data.map((x) => Math.abs(sum - x * n)); // 절대값(총합 - 요소 * 요소의 총 갯수) 차이 배열
+const min = Math.min(...diff);
+const index = diff.findIndex((x) => x === min); // 차이가 가장 작은 수의 index를 반환
+
+console.log(index+1, data[index])
