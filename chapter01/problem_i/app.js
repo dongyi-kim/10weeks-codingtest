d95d6e3b1d7376d913bafae5f12d09b8bb39710e
Sherry90 <hhlsdkm@gmail.com>
2023-04-23 00:04:24 +0900

    1I

--- chapter01/problem_i/app.js
+++ chapter01/problem_i/app.js
@@ -1,32 +1,24 @@
-function selection_sort(data, length)
-{
-  for(var i = 0 ; i < data.length ; i++ )
-  {
-    for (var j = i+1 ; j < data.length ; j++ )
-    {
-      if ( data[i] > data[j] ) {
-        var temp = data[i];
-        data[i] = data[j];
-        data[j] = temp;
-      }
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const n = Number(input[0]);
+const data = input[1].split(" ").map(Number);
+
+const getMinIndexInRange = (data, begin, end) => {
+    let minIndex = begin;
+    for(let i = begin ; i <= end ; i++){
+        if(data[i] < data[minIndex]){
+            minIndex = i;
+        }
     }
-  }
+    return minIndex;
 }
 
-const input = [];
-require('readline')
-  .createInterface(process.stdin, {})
-  .on('line', function(line) {
-    input.push(line.trim());
-  })
-  .on('close', function() {
-    const n = parseInt(input[0]);
-    const data = input[1].split(" ").map(Number);
-    var answer = "";
+for(let i = 0 ; i < n ; i++) {
+    let minIndex = getMinIndexInRange(data, i, n-1);
+    let tmp = data[i];
+    data[i] = data[minIndex];
+    data[minIndex] = tmp;
+}
 
-    selection_sort(data, n);
-
-    for (var i=0; i<n; i++)
-      answer += data[i] + " ";
-		console.log(answer);
-	});
+console.log(data.join(" "));
