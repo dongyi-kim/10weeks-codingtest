cbaa4da2d4a099b270816c28b3f2c88a37504a93
dongyi <dykim@codingmonster.net>
2024-03-25 12:12:29 +0900

    Change

--- chapter06/problem_b/Main.java
+++ chapter06/problem_b/Main.java
@@ -4,6 +4,8 @@ import java.lang.*;
 public class Main {
 	public static final Scanner scanner = new Scanner(System.in);
 
+
+
 	/**
 	 * @brief   시작 기둥(from)으로부터 목적지 기둥(to)까지 num개의 원반을 모두 옮기는 최소 이동 횟수를 계산하는 함수
 	 *
@@ -14,14 +16,15 @@ public class Main {
 	 * @param to        원반들을 모두 옮길 기둥
 	 * @return          모든 원반을 옮길 수 있는 최소 이동 횟수
 	 */
-	public static int getMinimumMove(int num, Stack<Disk> from, Stack<Disk> buffer, Stack<Disk> to) {
+	public static int getMinimumMove(int num, Bar from, Bar buffer, Bar to) {
 		if (num == 0) {
 			// 원판이 없다면 당연히 아무 이동도 필요하지 않다
 			return 0;
 		}else if(num == 1){
 			// 원판이 하나 뿐이라면 그냥 이동하면 된다
-			Disk d = from.pop();
-			to.push(d);
+			Disk d = from.stack.pop();
+			to.stack.push(d);
+			// System.out.println(from.name + " -> " + to.name);
 			return 1;
 		}
 
@@ -39,17 +42,19 @@ public class Main {
 		return move;
 	}
 
+
+
 	public static void main(String[] args) throws Exception {
 		int N = scanner.nextInt();
 
-		Stack<Disk> A = new Stack<>();
-		Stack<Disk> B = new Stack<>();
-		Stack<Disk> C = new Stack<>();
+		Bar A = new Bar("A");
+		Bar B = new Bar("B");
+		Bar C = new Bar("C");
 
 		// 첫 번째 기둥에 N개의 원판을 쌓는다
 		for (int i = N; i >= 1; i -= 1) {
 			Disk d = new Disk(i);
-			A.push(d);
+			A.stack.push(d);
 		}
 
 		int answer = getMinimumMove(N, A, B, C);
@@ -58,6 +63,14 @@ public class Main {
 	}
 }
 
+class Bar{
+	String name;
+	Stack<Disk> stack;
+	Bar(String name){
+		this.name = name;
+		this.stack = new Stack<>();
+	}
+}
 
 class Disk {
 	final int size;
